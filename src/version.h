008256a30e4de694cc74aeade4906c304a52b75d
F2re <f2re@users.noreply.github.com>
2026-08-07 19:46:37 +0300

    Expose firmware version metadata

---
+++ src/version.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef HYDRO_VERSION
+#define HYDRO_VERSION "dev"
+#endif
+
+#ifndef HYDRO_BUILD_SHA
+#define HYDRO_BUILD_SHA "unknown"
+#endif
+
+#define HYDRO_PRODUCT_NAME "HydroESP-C3"
+#define HYDRO_API_VERSION  2
