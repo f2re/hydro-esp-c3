2f3d9a9e5876dcfcb569159a806e3038826f456b
F2re <f2re@users.noreply.github.com>
2026-08-07 22:00:40 +0300

    Persist automation and hydraulic calibration settings

--- src/config_storage.h
+++ src/config_storage.h
@@ -7,6 +7,9 @@ struct Config {
     String wifi_ssid;
     String wifi_pass;
     int timezone_offset = TIMEZONE_OFFSET;
+    bool automation_enabled = true;
+    uint32_t pump_flow_ml_min = 0;
+    uint8_t delivery_efficiency_pct = 85;
     WateringSlot schedule[MAX_SCHEDULE_SLOTS]{};
     uint8_t schedule_count = 0;
 };
