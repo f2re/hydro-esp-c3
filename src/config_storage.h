8d3282269743a6e44085131d7210545056c41187
F2re <f2re@users.noreply.github.com>
2026-08-07 22:28:05 +0300

    Persist calibration protocol version

--- src/config_storage.h
+++ src/config_storage.h
@@ -10,6 +10,7 @@ struct Config {
     bool automation_enabled = true;
     uint32_t pump_flow_ml_min = 0;
     uint8_t delivery_efficiency_pct = 85;
+    uint8_t calibration_protocol_version = 0;
     uint8_t calibration_sample_count = 0;
     uint16_t calibration_cv_x100 = 0;
     uint32_t calibration_local_epoch = 0;
